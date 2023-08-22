baffea9429c0c3856591fe7c68956204b667d3bd
anthonellaeid <anthonellaeid@gmail.com>
2023-08-22 20:05:41 +0300

    implement api registration and authentication

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        // Log::info("testtttt");
+        $credentials = $request->only(['email', 'password']);
+
+        if(Auth::attempt($credentials))
+        {
+            $user = Auth::user();
+            $access_token = $user->createToken('authToken')->plainTextToken;
+            return response()->json(
+                [
+                    'success'=>true,
+                    'token'=>$access_token,
+                    'message'=>'User logged in successfully'
+                ]
+                );
+        }else{
+            return response()->json(
+                [
+                    'success'=>false,
+                    'message'=>'Wrong username or password'
+                ]
+                );
+        }
+    }
+
+
+    public function register(Request $request){
+
+        // Log::info("testtttt");
+        // Log::info($request);
+            // dd($request->all());
+        $user = User::where('email', $request->email)->first();
+
+        if(is_null($user))
+        {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json(
+                [
+                    'status'=>true,
+                    'message'=>'User Created Succesfully',
+                    'token' => $token
+                ]
+                );
+        }else{
+            return response()->json(
+                [
+                    'status'=>false,
+                    'message'=>'User already exists',
+                    
+                ]
+                );
+        }
+    }
+}
