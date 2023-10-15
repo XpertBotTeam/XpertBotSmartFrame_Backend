aa607e03b21744e8eb3c69758091e670a5f6e2ae
anthonellaeid <anthonellaeid@gmail.com>
2023-10-15 19:50:50 +0300

    upload picture other method

--- app/Http/Controllers/API/PictureController.php
+++ app/Http/Controllers/API/PictureController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Picture;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class PictureController extends Controller
 {
@@ -19,16 +20,17 @@ class PictureController extends Controller
     public function addPicture(Request $request){
         
         if ($request->hasFile('image')) {
+
             $image = $request->file('image');
-            // $attachment = time() . "_" . trim($file->getClientOriginalName());
-            // $dirProd = public_path('/uploads/seeker_jobs/');
-            // $file->move($dirProd, $atachment);
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path('uploads\pictures'), $imageName);
+            $imageName = time() . "_" . trim($image->getClientOriginalName());
+            // $directory = public_path('/uploads/pictures/');
+            // $image->move($directory, $imageName);
+            
+            Storage::putFileAs('public',$image, $imageName);
 
             $picture = new Picture();
             $picture->picture_type = $request->picture_type;
-            $picture->image_path = "/uploads/pictures/".$imageName;
+            $picture->image_path = asset('public/storage/'.$imageName);//"/uploads/pictures/".$imageName;
             $picture->description = $request->description;
             $picture->price = $request->price;
             $picture->save();
